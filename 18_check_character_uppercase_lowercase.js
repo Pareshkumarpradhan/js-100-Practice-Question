2564c3bd8a41ff02cf28cc5371b3e2d1906f6558
Paresh Kumar Pradhan <pareshkupradhan789@gmail.com>
2024-04-15 20:58:47 +0530

    Q 18 is added

---
+++ 18_check_character_uppercase_lowercase.js
@@ -0,0 +1,18 @@
+// 18: write a function to check if a character is uppercase or lowercase
+
+
+const isUpperCase = (char) => {
+//    if(char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90){
+//     return true;
+//    }
+//    return false;
+
+
+//second method
+return char === char.toUpperCase();
+}
+
+
+
+console.log(isUpperCase("s"));  // output: true
+// console.log(isLowerCase("b"));  // output: true
